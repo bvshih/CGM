7feab53f1ba6f6661e5de8646be5040ea4670f66
Beverly <bv.shih@gmail.com>
2023-04-27 14:34:26 -0500

    script to investigate disk to cgm movement using radial distribution

---
+++ scripts/disk.py
@@ -0,0 +1,150 @@
+import pynbody
+import pynbody.units as units
+
+import matplotlib.pyplot as plt
+import pynbody.plot.sph as sph
+
+import numpy as np
+
+def distance(x,y,z):
+    # coordinates should be from centered sim already 
+    return np.sqrt(x**2+y**2+z**2)
+
+def calculate_r(p):
+    r = [distance(*p['pos'][i]) for i in range(len(p))]
+    return r 
+    
+
+def radial_dist_plot(p, out_fn):
+    ''' 
+    radial distribution histogram
+    p: sim snap of gas particles only
+    '''
+    p_dict={}
+
+    cgm, disk=categorize_p(p)
+    print('p separated into cgm and disk')
+
+    r = {}
+    print('calculating r for',len(p_dict['cgm']),'cgm particles')
+    r['cgm'] = cgm['r']
+
+    print('calculating r for',len(p_dict['disk']),'disk particles')
+    r['disk'] = disk['r']
+    print('calculated r')
+
+    fig, ax = plt.subplots()
+
+    rmax = 300
+    disk_height = 5
+    bins = np.linspace(0, rmax, num = int(rmax/disk_height))
+    print('making radial dist plot')
+    ax.hist(r['cgm'], bins = bins, label = 'cgm', alpha = 0.5, color='coral' )
+    ax.hist(r['disk'], bins = bins, label = 'disk', alpha = 0.5, color='gold')
+
+    ax.semilogy()
+    ax.legend()
+    ax.set_xlabel('r [kpc]')
+
+    ax.set_title('z = 0.17')
+    plt.savefig(out_fn+'radial_dist.pdf')
+    print('radial dist plot saved')
+
+
+def categorize_p(p, rdisk="15 kpc", height='5 kpc'):
+    '''filter particles in disk vs cgm'''
+    disk_filt = pynbody.filt.Disc(rdisk, height, cen=(0,0,0))
+    cgm = p[~disk_filt]
+    disk = p[disk_filt]
+
+    return cgm, disk
+
+
+# file path for sim
+s1_fn = '/scratch/08263/tg875625/CGM/GMs/pioneer50h243.1536gst1bwK1BH/pioneer50h243.1536gst1bwK1BH.003456'
+z1 = '0.17'
+# file path for previous timestep 
+s2_fn = '/scratch/08263/tg875625/CGM/GMs/pioneer50h243.1536gst1bwK1BH/pioneer50h243.1536gst1bwK1BH.003072'
+z2='0.29'
+out_fn = '/scratch/08263/tg875625/CGM/plots/disk_z'+z1+'_z'+z2+'_'
+
+# load previous timestep
+s2 = pynbody.load(s2_fn)
+s2.physical_units()
+
+h1_s2 = s2.halos()[1]
+
+pynbody.analysis.angmom.faceon(h1_s2)
+print('loaded and rotated s2')
+
+# filter
+rdisk = "15 kpc"
+height = "5 kpc"
+disk_filt = pynbody.filt.Disc(rdisk, height, cen=(0,0,0))  
+
+temp_filt = pynbody.filt.LowPass('temp', '1.2e4 K')
+
+amu = units.NamedUnit("amu", 1.66e-27*units.kg)
+h1_s2.g['rho'].convert_units('amu cm**-3')  # convert array to new units
+
+rho_filt = pynbody.filt.HighPass('rho', '0.1 amu cm**-3')
+
+# metals_filt = pynbody.filt.HighPass('metals', 0.0134)
+
+# cool dense gas in disk of previous timestep
+d1_s2 = h1_s2.g[disk_filt & temp_filt & rho_filt]
+print('filtered s2')
+
+# load sim 2
+s1 = pynbody.load(s1_fn)
+s1.physical_units()
+
+h1_s1 = s1.halos()[1]
+
+pynbody.analysis.angmom.faceon(h1_s1)
+print('loaded and rotated s1')
+
+# bridge 
+b = s1.bridge(s2)
+print('briged')
+
+# cool dense gas disk particles from sim 2 in sim 1 
+d1_s1 = b(d1_s2)
+
+# radial dist plot 
+radial_dist_plot(d1_s1.g, out_fn)
+
+# s1_min = min(d1_s1.g['rho'])
+# s1_max = max(d1_s1.g['rho'])  
+
+# s2_min = min(d1_s2.g['rho'])
+# s2_max = max(d1_s2.g['rho'])
+
+# fig, ax = plt.subplots(ncols=2, nrows=2, sharex=True, sharey=True)
+# im1 = sph.image(d1_s1.g, qty='rho', vmin=s1_min, vmax=s1_max, width = '300 kpc', cmap='Purples', subplot = ax[0,0],ret_im=True, show_cbar=False) 
+# im2 = sph.image(d1_s2.g, qty='rho', vmin=s2_min, vmax=s2_max, width = '300 kpc', cmap='Blues', subplot = ax[1,0], ret_im=True, show_cbar=False) 
+# print('faceon images made')
+
+# pynbody.analysis.angmom.sideon(h1_s1)
+# print('rotated s1 to sideon')
+# pynbody.analysis.angmom.sideon(h1_s2)
+# print('rotated s2 to sideon')
+
+# sph.image(d1_s1.g, qty='rho',  vmin=s1_min, vmax=s1_max, width = '300 kpc', cmap='Purples', subplot = ax[0,1], show_cbar=False) 
+# sph.image(d1_s2.g, qty='rho',   vmin=s2_min, vmax=s2_max, width = '300 kpc', cmap='Blues', subplot = ax[1,1], show_cbar=False) 
+# print('sideon images made')
+
+# plt.subplots_adjust(right=0.8)
+# cax1 = plt.axes([0.85, 0.55, 0.03, 0.3])
+# cax2 = plt.axes([0.85, 0.1, 0.03, 0.3])
+
+# plt.colorbar(im1, cax=cax1, label='amu cm**-3')
+# plt.colorbar(im2, cax=cax2, label='amu cm**-3')
+
+# ax[1,0].set_ylabel('z/kpc')
+# ax[0,0].set_ylabel('z/kpc')
+
+# ax[0,0].text(-150,150,'z = 0.17')
+# ax[1,0].text(-150,150,'z = 0.25')
+# plt.savefig(out_fn+'sph_img.pdf')
+# print('sph image plot complete')
